6c51d2b651ceb0ce9ec80df84fc162bea5efc614
anandS1207 <sahukari.anandarao@gmail.com>
2019-03-29 17:46:02 +0530

    game cards are added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { GameCardsComponent } from './game-cards/game-cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    GameCardsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/game-cards/game-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'game-cards',
+  templateUrl: './game-cards.component.html',
+  styleUrls: ['./game-cards.component.css']
+})
+export class GameCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
